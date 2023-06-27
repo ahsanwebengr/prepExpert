f93a1c6178e8b9b1d258ccae5604d9767f21980f
ahsanwebengr <ahsanwebengr@gmail.com>
2023-06-27 18:50:16 +0500

    upload whole file

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,65 @@ document.addEventListener('click', function (event) {
 
   // If the clicked element is outside the mobile navigation and hamburger menu, close the mobile navigation
   if (!isClickInsideNav && !isClickInsideMenu) {
-    mobileNav.classList.remove('active');
-    hamburgerMenu.classList.toggle('close');
+    if (mobileNav.classList.contains('active')) {
+      mobileNav.classList.remove('active');
+      hamburgerMenu.classList.toggle('close');
+    }
   }
+});
+
+const tabs = document.querySelectorAll('[data-tabs-toggle]');
+
+tabs.forEach(function (tabsToggleEl) {
+  const tabsToggleElementsId = tabsToggleEl.getAttribute('id');
+  const tabsToggleElements = document.querySelectorAll(
+    '#' + tabsToggleElementsId + ' [role="tab"]'
+  );
+  var activeTabToggleEl = null;
+  var activeTabContentEl = null;
+
+  tabsToggleElements.forEach(function (tabToggleEl, index) {
+    tabToggleEl.addEventListener('click', function (event) {
+      var tabToggleEl = event.target;
+      var tabTargetSelector = tabToggleEl.getAttribute('data-tabs-target');
+      var tabContentEl = document.querySelector(tabTargetSelector);
+
+      // don't do anything if it's already active
+      if (tabToggleEl !== activeTabToggleEl) {
+        // find currently active tab toggle and content if not set
+        if (!activeTabToggleEl && !activeTabContentEl) {
+          activeTabToggleEl = document.querySelector(
+            '#' + tabsToggleElementsId + ' [aria-selected="true"]'
+          );
+          activeTabContentEl = document.querySelector(
+            activeTabToggleEl.getAttribute('data-tabs-target')
+          );
+        }
+
+        // show and activate tab
+        tabToggleEl.classList.add('active');
+        tabToggleEl.setAttribute('aria-selected', true);
+        tabContentEl.classList.remove('hidden');
+
+        // hide and deactive currently active tab toggle and content
+        activeTabToggleEl.setAttribute('aria-selected', false);
+        activeTabToggleEl.classList.remove('active');
+        activeTabContentEl.classList.add('hidden');
+
+        // set currently active toggle and content tabs
+        activeTabToggleEl = tabToggleEl;
+        activeTabContentEl = tabContentEl;
+      }
+    });
+
+    // Set the first tab as active by default
+    if (index === 0) {
+      tabToggleEl.classList.add('active');
+      tabToggleEl.setAttribute('aria-selected', true);
+      activeTabToggleEl = tabToggleEl;
+      activeTabContentEl = document.querySelector(
+        tabToggleEl.getAttribute('data-tabs-target')
+      );
+    }
+  });
 });
